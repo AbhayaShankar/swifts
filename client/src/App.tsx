43c5049850f7ef3d0f0c7e3dff4553dad26f75f1
Abhaya <shankarabhaya9@gmail.com>
2023-12-25 23:56:37 +0530

    fix: fixed routing using react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,18 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import Chat from "./pages/Chat.tsx";
 import Wrapper from "./layout.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Chat />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "*",
-    element: <Navigate to={"/"} />,
-  },
-]);
-
 function App(): JSX.Element {
   return (
     <Wrapper>
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Wrapper>
   );
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,32 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="px-10 py-3 border-white/20 border-b-[1px]">
+      <div className="flex items-center justify-between h-6">
+        <h1>
+          <Link to={"/"}>Swifts Chat</Link>
+        </h1>
+        <div className="flex items-center gap-10 ">
+          <Link
+            to={"/login"}
+            className="text-white/60 hover:text-white transition-all duration-100 ease-in"
+          >
+            Login
+          </Link>
+          <Link
+            to={"/register"}
+            className="text-white/60 hover:text-white transition-all duration-100 ease-in"
+          >
+            Register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
