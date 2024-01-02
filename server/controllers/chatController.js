fdfb07bbd141069e6b819b8994df3dc3b29205ec
Abhaya <shankarabhaya9@gmail.com>
2024-01-02 12:39:24 +0530

    build: chat apis

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,4 +1,4 @@
-// Chat Controllers
+// Chat Controller
 
 // 1. createChat -- /chats
 // 2. findAllUserChats -- /:userId (Finding all chats for that userId)
@@ -28,8 +28,32 @@ const createChat = async (req, res) => {
   }
 };
 
-const findAllUserChats = async (req, res) => {};
+const findAllUserChats = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const chats = await chatModel.find({
+      members: { $in: [userId] },
+    });
 
-const findUserChat = async (req, res) => {};
+    res.status(200).json(chats);
+  } catch (error) {
+    console.log("findAllUserChatsError", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const findUserChat = async (req, res) => {
+  const { firstId, secondId } = req.params;
+  try {
+    const chat = await chatModel.findOne({
+      members: { $all: [firstId, secondId] },
+    });
+
+    res.status(200).json(chat);
+  } catch (error) {
+    console.log("findUserChatError", error);
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = { createChat, findAllUserChats, findUserChat };
